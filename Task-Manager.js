41765fb4965b9d604c2d0ed68387af36b6a10429
BIHF <imeshahansani2003@gmail.com>
2025-11-17 22:49:59 +0530

    Initial commit of task manager app

---
+++ Task-Manager.js
@@ -0,0 +1,15 @@
+import readline from "readline";
+const r1 = readline.createInterface({
+    input:process.stdin, 
+    output:process.stdout
+})
+let tasks = [];
+
+function showMenu(){
+    console.log("\nWelcome to Task Manager!");
+    console.log("1.List tasks");
+    console.log("2. Add new task");
+    console.log("3. Mark task as complete");
+    console.log("4. Delete task");
+    console.log("5. Exit");
+}
