e021f82a2385c60b52223b52a753f3f30c62739c
BIHF <imeshahansani2003@gmail.com>
2025-11-19 22:45:05 +0530

    update

--- Task-Manager.js
+++ Task-Manager.js
@@ -1,4 +1,5 @@
 import readline from "readline";
+import CacheHandler from "undici-types/cache-interceptor";
 const r1 = readline.createInterface({
     input:process.stdin, 
     output:process.stdout
@@ -6,11 +7,23 @@ const r1 = readline.createInterface({
 let tasks = [];
 
 function showMenu(){
-    console.log("                          ");
     console.log("\nWelcome to Task Manager!");
     console.log("1.List tasks");
     console.log("2. Add new task");
     console.log("3. Mark task as complete");
     console.log("4. Delete task");
     console.log("5. Exit");
+    r1.question("Enter your choice: ",handMenu);
 }
+
+function handleMenu(choice) {
+  switch (choice) {
+    case "1": listTasks(); break;
+    case "2": addNewTasks(); break;
+    case "3": markTaskasComplete(); break;
+    case "4": deleteTask(); break;
+    case "5": exit(); break;
+    default: console.log("Invalid choice"); showMenu();
+  }
+}
+
