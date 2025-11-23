04df277671e2f4c53d29e3783c5de1d14620c1d3
BIHF <imeshahansani2003@gmail.com>
2025-11-23 14:26:07 +0530

    Updated

--- taskManager.js
+++ taskManager.js
@@ -1,3 +1,4 @@
+import { Console } from "console";
 import readline from "readline";
 const r1 = readline.createInterface({
     input:process.stdin, 
@@ -31,13 +32,44 @@ showMenu();
 function listTasks() {
     console.log("\nYour Tasks:");
 
+    if (tasks.length == 0){
+        Console.log("No tasks yet.");
+    } else {
+        for (let i = 0; i< tasks.length; i++){
+            let task = tasks[i];
+            let status = task.completed ? "[✔]" : "[ ]";
+
+            console.log((i+1) + "." + status + " " + task.name);
+        }
+    }
+
     showMenu();
 }
 
 function addNewTasks(){ 
     r1.question("Enter new task name:", function(taskName){
+        if (taskName.trim() === ""){
+            console.log("Task name cannot be empty!");
+            return;
+        }
     tasks.push({ name: taskName, completed: false });
         console.log("✅ Task added!\n");
         showMenu();
     });
+}
+
+function markTaskasComplete(){
+    r1.question("Enter task number to complete: ", function(num){
+        let index = Number(num) - 1;
+
+        if (index < 0 || index >= tasks.length) {
+            console.log("Invail task number!");
+            return;
+        }
+        tasks[index].completed = true;
+        console.log("✔ Task marked as complete!")
+        showMenu();
+    });
+
+    
 }
